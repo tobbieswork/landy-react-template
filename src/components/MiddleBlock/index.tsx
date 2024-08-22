cba39cffa21d3e9a6a3c97cbaf04ca9f80c27799
Tobbies Nguyen <dangphat.job@gmail.com>
2024-08-22 11:58:33 +0700

    Submit task 1

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Row, Col } from "antd";
 import { withTranslation, TFunction } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
@@ -12,12 +13,16 @@ interface MiddleBlockProps {
 }
 
 const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
+  const [buttonText, setButtonText] = useState(button);
   const scrollTo = (id: string) => {
     const element = document.getElementById(id) as HTMLDivElement;
     element.scrollIntoView({
       behavior: "smooth",
     });
   };
+  const handleChangeButtonText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setButtonText(e.target.value);
+  }
   return (
     <MiddleBlockSection>
       <Slide direction="up" triggerOnce>
@@ -26,9 +31,10 @@ const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
             <Col lg={24} md={24} sm={24} xs={24}>
               <h6>{t(title)}</h6>
               <Content>{t(content)}</Content>
+              <input type="text" value={buttonText} onChange={handleChangeButtonText} />
               {button && (
                 <Button name="submit" onClick={() => scrollTo("mission")}>
-                  {t(button)}
+                  {buttonText ? t(buttonText) : 'Get started'}
                 </Button>
               )}
             </Col>
